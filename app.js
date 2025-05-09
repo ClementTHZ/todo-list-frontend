c8b926e3dfdc8c09e29d70b9bd67d560e281b7da
Clément THOREZ <cthorez@selectour.fr>
2025-05-09 15:10:33 +0200

    HTML + méthode JS natif + formulaire INPUT

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const addButton = document.getElementById("add-button")
+addButton.addEventListener("click", (event) =>{
+  // event.preventDefault(); 
+  const inputTitle = document.getElementById("input-title"); 
+  const title = inputTitle.value;  
+  createTask(title); 
+})
+
 async function getData() {
   const url = "http://localhost:5000/tasks"; 
   try 
@@ -8,16 +16,28 @@ async function getData() {
     console.log(data)
 
     const taskList = document.getElementById("task-list"); 
+
     data.forEach(task => {
         const taskId = task.id; 
+
         const li = document.createElement("li"); 
-        li.textContent = `${task.title}`; 
-        const deleteButton = document.createElement("button"); 
-        deleteButton.textContent = "delete"; 
-        deleteButton.addEventListener("click", () => {
-            handleDelete(taskId)
+        li.classList.add("list-unstyled")
+        li.textContent = `${task.title} - `;
+
+        
+        const deleteTrash = document.createElement("i"); 
+        deleteTrash.classList.add("fa-solid");
+        deleteTrash.classList.add("fa-trash");
+        deleteTrash.classList.add("text-red-500");
+        deleteTrash.classList.add("hover:text-red-800");
+        deleteTrash.addEventListener("click", () => {
+            handleDelete("tasks", taskId)
         }); 
-        li.appendChild(deleteButton); 
+        const a = document.createElement("a"); 
+        a.href = ""; 
+        a.appendChild(deleteTrash)
+
+        li.appendChild(a); 
         taskList.appendChild(li); 
     })
   } 
@@ -27,8 +47,36 @@ async function getData() {
 }
 }
 
-function handleDelete(taskIndex) {
-    console.log(`index: ${taskIndex}`)
+async function handleDelete(table, id) {
+  const url = `http://localhost:5000/${table}/${id}` 
+  try {
+    const response = await fetch(url, {
+      method: "DELETE"
+    }); 
+    console.log(response.status); 
+    location.reload(); 
+  } catch (error) {
+    console.log(error.message); 
+  }
+}
+
+async function createTask(title){
+const newTask = {
+  title: title
+}
+try {
+  const response = await fetch("http://localhost:5000/tasks/new", {
+    method: "POST", 
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newTask)
+  })
+  console.log("Task has been created !")
+  console.log(response.status) 
+} catch (error) {
+  console.log(error.message)
+}
 }
 
 getData(); 
